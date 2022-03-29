1705c97179c61e35f0ed5f6ffc8c0148ce1e5c30
DerCDon <mkuschel@tso.de>
2022-03-29 12:25:31 +0200

    Copied Code from last Project

--- main.c
+++ main.c
@@ -13,8 +13,78 @@
  **/
 int day_of_the_year(int day, int month, int year)
 {
+    int i;
+    int days_per_month[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int days = 0;
+
+    printf(" *** Tag des Jahres ***\n");
+
+    while(1)
+    {
+        printf("Tag:");
+        scanf("%i",&day);
+        printf("Monat:");
+        scanf("%i",&month);
+        printf("Jahr:");
+        scanf("%i",&year);
+        if (month == 2)
+        {
+            if (schaltjahr(year) == 1)
+            {
+                if(day > 29)
+                {
+                    printf("Ungueltiger Tag\n");
+                    continue;
+                }
+            }
+
+        }
+        if (day > days_per_month[month +1])
+        {
+            printf("Ungueltiger Tag\n");
+                            continue;
+        }
+        if (month > 12)
+        {
+            printf("Ungueltiger Monat\n");
+                            continue;
+        }
+        break;
+    }
+    if (month>1)
+    {
+        for(i=0; i< month-1; i++)
+        {
+            if(schaltjahr(year) == 1)
+            {
+                days_per_month[1] = 29;
+            }
+            days += days_per_month[i];
+        }
+        days += day;
+
+    }
+    else
+    {
+        days = day;
+    }
+    printf("Der %i.%i.%i ist der %i. des Jahres",day,month,year,days);
+
 
     return 0;
+}
+
+int schaltjahr(int year)
+{
+    if (year%4 == 0)
+    {
+        if (year%100 != 0 || year%400 == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+
 }
 
 int main()
